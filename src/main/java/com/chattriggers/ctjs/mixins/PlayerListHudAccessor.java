8481d335035ff07fff2d15680ea85e289e3e6577
Matthew Olsson <matthewcolsson@gmail.com>
2023-05-19 07:17:22 -0700

    Fix PlayerListHudAccessor

--- src/main/java/com/chattriggers/ctjs/mixins/PlayerListHudAccessor.java
+++ src/main/java/com/chattriggers/ctjs/mixins/PlayerListHudAccessor.java
@@ -1,10 +1,11 @@
 package com.chattriggers.ctjs.mixins;
 
+import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(PlayerListHudAccessor.class)
+@Mixin(PlayerListHud.class)
 public interface PlayerListHudAccessor {
     @Accessor
     Text getHeader();
