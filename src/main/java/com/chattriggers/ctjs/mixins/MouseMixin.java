6f1604114afc402508342df9cc300d72304053ed
Cam Walter <cam.n.walter@gmail.com>
2023-05-19 23:49:24 -0500

    IRegister: Remove guiMouseRelease
    GuiMouseClick encompasses both the presses and releases now

--- src/main/java/com/chattriggers/ctjs/mixins/MouseMixin.java
+++ src/main/java/com/chattriggers/ctjs/mixins/MouseMixin.java
@@ -2,15 +2,26 @@ package com.chattriggers.ctjs.mixins;
 
 import com.chattriggers.ctjs.minecraft.CTEvents;
 import com.chattriggers.ctjs.minecraft.listeners.MouseListener;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.gui.screen.Screen;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
 public class MouseMixin {
+    @Shadow
+    private int activeButton;
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
     @Inject(
         method = "onMouseButton",
         at = @At(
@@ -33,5 +44,19 @@ public class MouseMixin {
     )
     void injectOnMouseScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
         MouseListener.onRawMouseScroll(horizontal, vertical);
+    }
+
+    @Inject(
+        method = "method_1602(Lnet/minecraft/client/gui/screen/Screen;DDDD)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/screen/Screen;mouseDragged(DDIDD)Z"
+        ),
+        cancellable = true
+    )
+    void injectOnMouseDrag(Screen screen, double d, double e, double f, double g, CallbackInfo ci) {
+        if (client.currentScreen != null) {
+            CTEvents.GUI_MOUSE_DRAG.invoker().process(f, g, d, e, activeButton, client.currentScreen, ci);
+        }
     }
 }
