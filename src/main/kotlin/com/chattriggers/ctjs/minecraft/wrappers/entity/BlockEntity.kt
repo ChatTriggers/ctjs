f646cc5f696a6d16fa5d117c2c734921799e2743
Cam Walter <cam.n.walter@gmail.com>
2023-06-17 17:06:35 -0500

    BlockEntity: Add getBlockType and getBlock

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/wrappers/entity/BlockEntity.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/wrappers/entity/BlockEntity.kt
@@ -1,7 +1,10 @@
 package com.chattriggers.ctjs.minecraft.wrappers.entity
 
+import com.chattriggers.ctjs.minecraft.wrappers.world.block.Block
 import com.chattriggers.ctjs.minecraft.wrappers.world.block.BlockPos
+import com.chattriggers.ctjs.minecraft.wrappers.world.block.BlockType
 import com.chattriggers.ctjs.utils.MCBlockEntity
+import net.minecraft.block.entity.BlockEntityType
 
 // TODO(breaking): Renamed from TileEntity to BlockEntity
 class BlockEntity(val blockEntity: MCBlockEntity) {
@@ -11,9 +14,13 @@ class BlockEntity(val blockEntity: MCBlockEntity) {
 
     fun getZ(): Int = getBlockPos().z
 
+    fun getBlockType(): BlockType = BlockType(BlockEntityType.getId(blockEntity.type)!!.toString())
+
     fun getBlockPos(): BlockPos = BlockPos(blockEntity.pos)
 
+    fun getBlock(): Block = Block(getBlockType(), getBlockPos())
+
     override fun toString(): String {
-        return "BlockEntity{x=${getX()}, y=${getY()}, z=${getZ()}}"
+        return "BlockEntity{type=${getBlockType()}, pos=(${getX()}, ${getY()}, ${getZ()})}"
     }
 }
