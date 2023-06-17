64d08c7aebccb5279fdb6cacaed707a0090c54d2
Matthew Olsson <matthewcolsson@gmail.com>
2023-06-17 14:06:57 -0700

    PreLaunch: Add an IMixinConfigPlugin to redirect stdout/stderr
    MC does it on its own, but it happens in its main() function, which
    doesn't get called until after mixin application. So we mimic its
    behavior ourselves.

--- src/main/kotlin/com/chattriggers/ctjs/launch/CTJSPreLaunch.kt
+++ src/main/kotlin/com/chattriggers/ctjs/launch/CTJSPreLaunch.kt
@@ -3,7 +3,6 @@ package com.chattriggers.ctjs.launch
 import com.chattriggers.ctjs.console.LogType
 import com.chattriggers.ctjs.console.printToConsole
 import com.chattriggers.ctjs.console.printTraceToConsole
-import com.chattriggers.ctjs.engine.module.ModuleManager
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
 
 class CTJSPreLaunch : PreLaunchEntrypoint {
@@ -13,17 +12,6 @@ class CTJSPreLaunch : PreLaunchEntrypoint {
             "Uncaught exception in thread \"${thread.name}\"".printToConsole(logType = LogType.ERROR)
             exception.printTraceToConsole()
             prevHandler.uncaughtException(thread, exception)
-        }
-
-        Mappings.initialize()
-
-        // TODO: Threaded setup?
-        ModuleManager.setup()
-
-        try {
-            // TODO: Dynamic mixins
-        } catch (e: Throwable) {
-            IllegalStateException("Error generating dynamic mixins", e).printTraceToConsole()
         }
     }
 }
