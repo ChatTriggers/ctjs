9f336305c8d123f47ebf704429449fb0d5592d60
Matthew Olsson <matthewcolsson@gmail.com>
2023-06-17 14:28:02 -0700

    JSLoader: Add implementation for dynamic mixin loading
    Also adds mixinProvidedLibs.js, which will contain the glue between the
    JSLoader and the JS language.

--- src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSLoader.kt
+++ src/main/kotlin/com/chattriggers/ctjs/engine/langs/js/JSLoader.kt
@@ -7,6 +7,7 @@ import com.chattriggers.ctjs.engine.ILoader
 import com.chattriggers.ctjs.engine.langs.Lang
 import com.chattriggers.ctjs.engine.module.Module
 import com.chattriggers.ctjs.engine.module.ModuleManager.modulesFolder
+import com.chattriggers.ctjs.launch.IInjector
 import com.chattriggers.ctjs.launch.Mixin
 import com.chattriggers.ctjs.launch.MixinCallback
 import com.chattriggers.ctjs.triggers.Trigger
@@ -18,6 +19,8 @@ import org.mozilla.javascript.commonjs.module.Require
 import org.mozilla.javascript.commonjs.module.provider.StrongCachingModuleScriptProvider
 import org.mozilla.javascript.commonjs.module.provider.UrlModuleSourceProvider
 import java.io.File
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType
 import java.net.URI
 import java.net.URL
 import java.util.concurrent.ConcurrentHashMap
@@ -34,13 +37,26 @@ object JSLoader : ILoader {
     private lateinit var evalContext: Context
     private lateinit var scope: Scriptable
     private lateinit var require: CTRequire
+    private lateinit var moduleProvider: ModuleScriptProvider
+
+    private var mixinLibsLoaded = false
+    private var mixinsFinalized = false
+    private var nextMixinId = 0
+    private val mixinIdMap = mutableMapOf<Int, MixinCallback>()
+    private val mixins = mutableMapOf<Mixin, ILoader.MixinDetails>()
+
+    private val INVOKE_MIXIN_CALL = MethodHandles.lookup().findStatic(
+        JSLoader::class.java,
+        "invokeMixin",
+        MethodType.methodType(Any::class.java, Callable::class.java, Array<Any?>::class.java),
+    )
 
     override fun exec(type: TriggerType, args: Array<out Any?>) {
         triggers[type]?.forEach { it.trigger(args) }
     }
 
     override fun addTrigger(trigger: Trigger) {
-        triggers.getOrPut(trigger.type, ::newTriggerSet).add(trigger)
+        triggers.getOrPut(trigger.type, ::ConcurrentSkipListSet).add(trigger)
     }
 
     override fun clearTriggers() {
@@ -51,13 +67,56 @@ object JSLoader : ILoader {
         triggers[trigger.type]?.remove(trigger)
     }
 
-    private fun newTriggerSet() = ConcurrentSkipListSet<Trigger>()
-
     override fun setup(jars: List<URL>) {
-        instanceContexts(jars)
+        // Ensure all active mixins are invalidated
+        // TODO: It would be nice to do this, but it's possible to have a @Redirect or similar
+        //       mixin try to call it's handler between when we start loading and when we finish
+        //       loading, which would crash. So we need to be smarter about invalidation on load.
+        // mixinIdMap.values.forEach(MixinCallback::release)
+
+        JSContextFactory.addAllURLs(jars)
+
+        moduleContext = JSContextFactory.enterContext()
+        val sourceProvider = UrlModuleSourceProvider(listOf(modulesFolder.toURI()), listOf())
+        moduleProvider = StrongCachingModuleScriptProvider(sourceProvider)
+        scope = ImporterTopLevel(moduleContext)
+        require = CTRequire(moduleProvider)
+        require.install(scope)
+
+        Context.exit()
+
+        JSContextFactory.optimize = false
+        evalContext = JSContextFactory.enterContext()
+        Context.exit()
+        JSContextFactory.optimize = true
+
+        mixinLibsLoaded = false
+    }
+
+    override fun mixinSetup(modules: List<Module>): Map<Mixin, ILoader.MixinDetails> {
+        loadMixinLibs()
+
+        wrapInContext {
+            modules.forEach { module ->
+                try {
+                    val uri = File(module.folder, module.metadata.mixinEntry!!).normalize().toURI()
+                    require.loadCTModule(uri.toString(), uri)
+                } catch (e: Throwable) {
+                    e.printTraceToConsole(console)
+                }
+            }
+        }
+
+        mixinsFinalized = true
+        mixinLibsLoaded = true
+
+        return mixins
     }
 
     override fun entrySetup(): Unit = wrapInContext {
+        if (!mixinLibsLoaded)
+            loadMixinLibs()
+
         val moduleProvidedLibs = saveResource(
             "/assets/chattriggers/js/moduleProvidedLibs.js",
             File(modulesFolder.parentFile, "chattriggers-modules-provided-libs.js"),
@@ -128,31 +187,81 @@ object JSLoader : ILoader {
         }
     }
 
-    private fun instanceContexts(files: List<URL>) {
-        JSContextFactory.addAllURLs(files)
+    private fun loadMixinLibs() {
+        val mixinProvidedLibs = saveResource(
+            "/assets/chattriggers/js/mixinProvidedLibs.js",
+            File(modulesFolder.parentFile, "chattriggers-mixin-provided-libs.js"),
+            true,
+        )
 
-        moduleContext = JSContextFactory.enterContext()
-        scope = ImporterTopLevel(moduleContext)
-
-        val sourceProvider = UrlModuleSourceProvider(listOf(modulesFolder.toURI()), listOf())
-        val moduleProvider = StrongCachingModuleScriptProvider(sourceProvider)
-        require = CTRequire(moduleProvider)
-        require.install(scope)
-
-        Context.exit()
-
-        JSContextFactory.optimize = false
-        evalContext = JSContextFactory.enterContext()
-        Context.exit()
-        JSContextFactory.optimize = true
-    }
-
-    override fun mixinSetup(modules: List<Module>): Map<Mixin, ILoader.MixinDetails> {
-        TODO("Not yet implemented")
+        wrapInContext {
+            try {
+                moduleContext.evaluateString(
+                    scope,
+                    mixinProvidedLibs,
+                    "mixinProvided",
+                    1, null
+                )
+            } catch (e: Throwable) {
+                e.printTraceToConsole()
+            }
+        }
     }
 
     override fun invokeMixinLookup(id: Int): MixinCallback {
-        TODO("Not yet implemented")
+        val callback = mixinIdMap[id] ?: error("Unknown mixin id $id for loader ${this::class.simpleName}")
+
+        callback.handle = if (callback.method != null) {
+            try {
+                require(callback.method is Callable) {
+                    "The value passed to MixinCallback.attach() must be a function"
+                }
+
+                INVOKE_MIXIN_CALL.bindTo(callback.method)
+            } catch (e: Throwable) {
+                // TODO: More detail (how can we get the module name?)
+                "Error loading mixin callback".printToConsole(console)
+                e.printTraceToConsole(console)
+
+                MethodHandles.dropArguments(
+                    MethodHandles.constant(Any::class.java, null),
+                    0,
+                    Array<Any?>::class.java,
+                )
+            }
+        } else null
+
+        return callback
+    }
+
+    @JvmStatic
+    fun invokeMixin(func: Callable, args: Array<Any?>): Any? {
+        return wrapInContext {
+            func.call(Context.getCurrentContext(), scope, scope, args)
+        }
+    }
+
+    fun registerInjector(mixin: Mixin, injector: IInjector): MixinCallback? {
+        return if (mixinsFinalized) {
+            // We are reprocessing the mixin entry file from a ct reload, so we can
+            // just return the existing mixin callback. If none exists, then the user
+            // has added a mixin, which requires a reload
+
+            val existing = mixins[mixin]?.injectors?.find { it.injector == injector }
+            if (existing != null) {
+                existing
+            } else {
+                "A new injector mixin was registered at runtime. This will require a restart, and will " +
+                    "have no effect until then!".printToConsole(console)
+                null
+            }
+        } else {
+            val id = nextMixinId++
+            MixinCallback(id, injector).also {
+                mixinIdMap[id] = it
+                mixins.getOrPut(mixin, ILoader::MixinDetails).injectors.add(it)
+            }
+        }
     }
 
     class CTRequire(
