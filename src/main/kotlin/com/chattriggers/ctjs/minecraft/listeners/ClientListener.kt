937074ded1278547a2d349dccf425b3fb2bca6dc
Cam Walter <cam.n.walter@gmail.com>
2023-06-26 14:24:16 -0500

    ClientListener: Add toString method for PlayerInteractions

--- src/main/kotlin/com/chattriggers/ctjs/minecraft/listeners/ClientListener.kt
+++ src/main/kotlin/com/chattriggers/ctjs/minecraft/listeners/ClientListener.kt
@@ -279,12 +279,14 @@ object ClientListener : Initializer {
     }
 
     // TODO(breaking): Difference cases here
-    sealed class PlayerInteraction(val mainHand: Boolean) {
-        object AttackBlock : PlayerInteraction(true)
-        object AttackEntity : PlayerInteraction(true)
-        object BreakBlock : PlayerInteraction(true)
-        class UseBlock(hand: Hand) : PlayerInteraction(hand == Hand.MAIN_HAND)
-        class UseEntity(hand: Hand) : PlayerInteraction(hand == Hand.MAIN_HAND)
-        class UseItem(hand: Hand) : PlayerInteraction(hand == Hand.MAIN_HAND)
+    sealed class PlayerInteraction(val name: String, val mainHand: Boolean) {
+        object AttackBlock : PlayerInteraction("AttackBlock", true)
+        object AttackEntity : PlayerInteraction("AttackEntity", true)
+        object BreakBlock : PlayerInteraction("BreakBlock", true)
+        class UseBlock(hand: Hand) : PlayerInteraction("UseBlock", hand == Hand.MAIN_HAND)
+        class UseEntity(hand: Hand) : PlayerInteraction("UseEntity", hand == Hand.MAIN_HAND)
+        class UseItem(hand: Hand) : PlayerInteraction("UseItem", hand == Hand.MAIN_HAND)
+
+        override fun toString(): String = name
     }
 }
