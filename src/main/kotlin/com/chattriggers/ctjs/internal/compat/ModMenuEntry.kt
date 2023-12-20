6b2fd9d35dcba43d9027468b4a25c5d451258cac
Matthew Olsson <matthewcolsson@gmail.com>
2023-12-20 09:36:31 -0700

    Remove ModMenu for now
    This is blocking the merger of the 1.20.4 update, and needs to be
    resolved so we can migrate to the new Fabric loader.

--- src/main/kotlin/com/chattriggers/ctjs/internal/compat/ModMenuEntry.kt
+++ src/main/kotlin/com/chattriggers/ctjs/internal/compat/ModMenuEntry.kt
@@ -1,13 +1,14 @@
 package com.chattriggers.ctjs.internal.compat
 
-import com.chattriggers.ctjs.api.Config
-import com.terraformersmc.modmenu.api.ConfigScreenFactory
-import com.terraformersmc.modmenu.api.ModMenuApi
-
-internal class ModMenuEntry : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory {
-            Config.gui()
-        }
-    }
-}
+// TODO: Re-add this
+// import com.chattriggers.ctjs.api.Config
+// import com.terraformersmc.modmenu.api.ConfigScreenFactory
+// import com.terraformersmc.modmenu.api.ModMenuApi
+//
+// internal class ModMenuEntry : ModMenuApi {
+//     override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
+//         return ConfigScreenFactory {
+//             Config.gui()
+//         }
+//     }
+// }
