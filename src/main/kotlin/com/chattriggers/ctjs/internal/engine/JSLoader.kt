a1dbb78b428c86a2fa14e7dcc7e2a4f66c4bfbf4
Matthew Olsson <matthewcolsson@gmail.com>
2024-01-08 09:38:34 -0700

    JSLoader: Remove runtime field/method widener warnings
    ModuleManager already warns if the user imports a module with mixins, so
    not only is this warning incorrect (it printed every /ct load), it is
    useless as well

--- src/main/kotlin/com/chattriggers/ctjs/internal/engine/JSLoader.kt
+++ src/main/kotlin/com/chattriggers/ctjs/internal/engine/JSLoader.kt
@@ -265,25 +265,13 @@ object JSLoader {
     }
 
     fun registerFieldWidener(mixin: Mixin, fieldName: String, isMutable: Boolean) {
-        if (mixinsFinalized) {
-            if (mixins[mixin]?.fieldWideners?.contains(fieldName) != null) {
-                ("A new field widener was registered at runtime. This will require a restart, and will " +
-                    "have no effect until then!").printToConsole()
-            }
-        } else {
+        if (!mixinsFinalized)
             mixins.getOrPut(mixin, ::MixinDetails).fieldWideners[fieldName] = isMutable
-        }
     }
 
     fun registerMethodWidener(mixin: Mixin, methodName: String, isMutable: Boolean) {
-        if (mixinsFinalized) {
-            if (mixins[mixin]?.methodWideners?.contains(methodName) != null) {
-                ("A new method widener was registered at runtime. This will require a restart, and will " +
-                    "have no effect until then!").printToConsole()
-            }
-        } else {
+        if (!mixinsFinalized)
             mixins.getOrPut(mixin, ::MixinDetails).methodWideners[methodName] = isMutable
-        }
     }
 
     /**
